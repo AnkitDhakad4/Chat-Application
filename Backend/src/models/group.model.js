727e71c1a280b49a89ab0b4518f888e72faa7c2b
Ankit Dhakad <231220011@nitdelhi.ac.in>
2026-06-08 00:03:59 +0530

    group schema creation

---
+++ Backend/src/models/group.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const groupSchema=new mongoose.Schema({
+    groupName:{
+        type:String,
+        required:true
+    },
+    groupIcon:{
+        type:String
+    },
+    admin:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    members:[{type:mongoose.Schema.Types.ObjectId,ref:"User"}]
+},{timestamps:true})
+
+const Group=mongoose.model('Group',groupSchema)
+
+export default Group
